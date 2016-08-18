aa17494747ceb13278ee79312b45c5d24a7f6bbc
B4Bentley <B4Bentley>
2016-08-18 10:39:09 -0400

    skelton has been created in html

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyC2jMKydlt6G2C6CFdV6O-RzZGTv9AkhEI",
+		    authDomain: "traindb-3f4f7.firebaseapp.com",
+		    databaseURL: "https://traindb-3f4f7.firebaseio.com",
+		    storageBucket: "traindb-3f4f7.appspot.com",
+		  };
+		  firebase.initializeApp(config);
+
+		  var database = firebase.database();
