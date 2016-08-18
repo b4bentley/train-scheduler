74fe2e14bcd15f7f0562febd9fdaf6399cd80eab
B4Bentley <B4Bentley>
2016-08-18 11:28:34 -0400

    added math logic for time moments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -23,7 +23,7 @@ $('#trainSubmit').on('click', function(e){
     //create a new object train
     var newTrain = {
         name: trainName,
-        dest: destination,
+        destin: destination,
         first: firstTrain,
         freq: frequency,
     }
@@ -31,5 +31,45 @@ $('#trainSubmit').on('click', function(e){
     //push new train to firebase
     database.ref().push(newTrain);
 
+    //set input fields to blank
+    $('#trainName').val('');
+    $('#destination').val('');
+    $('#firstTrainTime').val('');
+    $('#frequency').val('');
+
 	return false;
+});
+
+database.ref().on('child_added', function(snapshot,value){
+
+    var trainName = snapshot.val().name;
+    var destination = snapshot.val().destin;
+    var firstTrain = snapshot.val().first;
+    var frequency = snapshot.val().freq;
+
+    //First time for intial value
+    var startTime = moment(firstTrain, 'hh:mm');
+    console.log("initial time is: " + moment(startTime).format('hh:mm'));
+
+    //current time to find out difference
+    var currentTime = moment();
+    console.log("current time is: " + moment(currentTime).format("hh:mm"));
+
+    //difference between initial and current
+    var difference = moment().diff(moment(startTime), "minutes");
+    console.log("difference in initial time and current time: " + diffTime);
+
+    //modular math to figure out time 
+    var remainder = difference % frequency;
+    console.log("the remainder is: " + remainder);
+
+    //minutes away time calculation
+    var minutesAway = frequency - remainder;
+    console.log("minutes till train: " + minutesAway);
+
+    //calculate the next train arrival time
+    var nextTrain = moment().add(minutesAway, "minutes");
+    console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+    $('.trainData').append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + moment(nextTrain).format("hh:mm") + "</td><td>" + minutesAway + "</td></tr>");
 });
